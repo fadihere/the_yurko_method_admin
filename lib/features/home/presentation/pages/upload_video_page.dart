6db619039775c457f5f372700dc6a8e2efa3c81b
mohsin@swaticorporation.com <pk.flutter@gmail.comm>
2024-01-26 01:13:51 +0500

    video done

--- lib/features/home/presentation/pages/upload_video_page.dart
+++ lib/features/home/presentation/pages/upload_video_page.dart
@@ -1,31 +1,24 @@
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:the_yurko_method/core/constants/app_colors.dart';
 import 'package:the_yurko_method/core/constants/app_style.dart';
 import 'package:the_yurko_method/core/widgets/app_button.dart';
 import 'package:the_yurko_method/core/widgets/text_form_field.dart';
-import 'package:the_yurko_method/features/home/presentation/controller/user_controller.dart';
 
 class UploadVideoPage extends StatefulWidget {
-  const UploadVideoPage({super.key});
+  const UploadVideoPage({Key? key}) : super(key: key);
 
   @override
   State<UploadVideoPage> createState() => _UploadVideoPageState();
 }
 
 class _UploadVideoPageState extends State<UploadVideoPage> {
-  Future<void> _getImage(ImageSource source) async {
-    final pickedFile = await ImagePicker().pickImage(source: source);
-
-    if (pickedFile == null) return;
-    final image = File(pickedFile.path);
-    final controller = Get.put(UserController());
-    controller.uploadProfile(image);
-  }
+  File? galleryFile;
+  final picker = ImagePicker();
+  Uint8List? imageBytes;
 
   @override
   Widget build(BuildContext context) {
@@ -38,116 +31,95 @@ class _UploadVideoPageState extends State<UploadVideoPage> {
           ),
         ),
       ),
-      body: Center(
-        child: SizedBox(
-          width: 324.w,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              SizedBox(
-                height: 20.h,
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          'Title',
-                          style: AppTextStyle.inter18Normal400(),
+              SizedBox(height: 20.h),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Title',
+                    style: AppTextStyle.inter18Normal400(),
+                  ),
+                  const CustomTextFormField(),
+                  SizedBox(height: 20.h), // Adjust as needed
+                  Text(
+                    'Description',
+                    style: AppTextStyle.inter18Normal400(),
+                  ),
+                  SizedBox(height: 8.h), // Adjust as needed
+                  TextFormField(
+                    maxLines: 3,
+                    cursorColor: AppColors.primary,
+                    decoration: InputDecoration(
+                      fillColor: const Color(0xffF1F1F1),
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 18,
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: AppColors.transparent,
                         ),
-                      ],
-                    ),
-                    const CustomTextFormField(),
-                    Row(
-                      children: [
-                        Text(
-                          'Description',
-                          style: AppTextStyle.inter18Normal400(),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: AppColors.primary,
                         ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 100.h,
-                      child: Expanded(
-                        child: TextFormField(
-                          maxLines: 3,
-                          cursorColor: AppColors.primary,
-                          decoration: InputDecoration(
-                            fillColor: const Color(0xffF1F1F1),
-                            contentPadding: const EdgeInsets.only(
-                              left: 18,
-                              right: 20,
-                              top: 12,
-                              bottom: 12,
-                            ),
-                            border: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: AppColors.transparent,
-                              ),
-                              borderRadius: BorderRadius.circular(8.0.r),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: AppColors.primary,
-                              ),
-                              borderRadius: BorderRadius.circular(8.0.r),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: AppColors.transparent,
-                              ),
-                              borderRadius: BorderRadius.circular(8.0.r),
-                            ),
-                            filled: true,
-                            suffixIconColor: const Color(0xff949393),
-                            floatingLabelStyle: const TextStyle(fontSize: 12),
-                            hintStyle: const TextStyle(
-                                fontSize: 14, color: Color(0xff949393)),
-                            prefixIconColor: const Color(0xff949393),
-                            labelStyle: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: AppColors.transparent,
                         ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      filled: true,
+                      suffixIconColor: const Color(0xff949393),
+                      floatingLabelStyle: const TextStyle(fontSize: 12),
+                      hintStyle: const TextStyle(
+                        fontSize: 14,
+                        color: Color(0xff949393),
+                      ),
+                      prefixIconColor: const Color(0xff949393),
+                      labelStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Row(
-                      children: [
-                        Text(
-                          'Video',
-                          style: AppTextStyle.inter18Normal400(),
+                  ),
+                  SizedBox(height: 20.h), // Adjust as needed
+                  Text(
+                    'Video',
+                    style: AppTextStyle.inter18Normal400(),
+                  ),
+                  SizedBox(height: 8.h), // Adjust as needed
+                  GestureDetector(
+                    onTap: () {
+                      _showPicker(context: context);
+                    },
+                    child: Container(
+                      width: 84.w,
+                      height: 84.h,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xffD9D9D9),
                         ),
-                      ],
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Center(
+                          child: galleryFile != null
+                              ? const Icon(Icons.play_arrow)
+                              : const Icon(Icons.add)),
                     ),
-                    Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            _showImageSourceBottomSheet();
-                          },
-                          child: Container(
-                            width: 84.w,
-                            height: 84.h,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: const Color(0xffD9D9D9),
-                              ),
-                              borderRadius: BorderRadius.circular(8.0.r),
-                            ),
-                            child: Center(
-                              child: Icon(
-                                Icons.add,
-                                color: const Color(0xffD9D9D9),
-                                size: 80.sp,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+              SizedBox(height: 180.h),
               AppButton(
                 text: 'Upload',
                 onTap: () {},
@@ -159,31 +131,55 @@ class _UploadVideoPageState extends State<UploadVideoPage> {
     );
   }
 
-  void _showImageSourceBottomSheet() {
+  void _showPicker({
+    required BuildContext context,
+  }) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return Wrap(
-          children: <Widget>[
-            // ListTile(
-            //   leading: const Icon(Icons.camera),
-            //   title: const Text('Camera'),
-            //   onTap: () {
-            //     Navigator.pop(context);
-            //     _getImage(ImageSource.camera);
-            //   },
-            // ),
-            ListTile(
-              leading: const Icon(Icons.image),
-              title: const Text('Select Video'),
-              onTap: () {
-                Navigator.pop(context);
-                _getImage(ImageSource.gallery);
-              },
-            ),
-          ],
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.photo_library),
+                title: const Text('Gallery'),
+                onTap: () {
+                  getVideo(ImageSource.gallery);
+                  Navigator.of(context).pop();
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Camera'),
+                onTap: () {
+                  getVideo(ImageSource.camera);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
         );
       },
     );
+  }
+
+  Future getVideo(
+    ImageSource img,
+  ) async {
+    final pickedFile = await picker.pickVideo(
+      source: img,
+      preferredCameraDevice: CameraDevice.front,
+      maxDuration: const Duration(minutes: 10),
+    );
+    XFile? xfilePick = pickedFile;
+    setState(() {
+      if (xfilePick != null) {
+        galleryFile = File(pickedFile!.path);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Nothing is selected')),
+        );
+      }
+    });
   }
 }
